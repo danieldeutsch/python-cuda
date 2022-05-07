fc9c08b020f09973599e46859d9b18c53eff4fd4
Daniel Deutsch <danfdeutsch@gmail.com>
2022-05-07 15:01:39 -0400

    Updating to v2 after previous versions broke

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 IMAGE_BASE_NAME="danieldeutsch/python"
+VERSION="v2"
 
 set -e
 
@@ -10,24 +11,24 @@ echo -e "## Supported tags\n" >>README_.md
 
 for py in "3.6" "3.7" "3.8" "3.9"; do
     echo -e "### Python $py\n" >>README_.md
-    for cuda in "11.0.3"; do
+    for cuda in "10.1" "10.2" "11.0.3" "11.1.1"; do
         echo -e "#### CUDA ${cuda}\n" >>README_.md
         IMAGE_PREFIX="${IMAGE_BASE_NAME}:${py}"
+        TAG="${IMAGE_PREFIX}-cuda${cuda}-base-${VERSION}"
 
-
-        echo "Building ${IMAGE_PREFIX}-cuda${cuda}-base"
+        echo "Building ${TAG}"
         docker build \
             --quiet \
             --build-arg PY="${py}" \
-            --tag "${IMAGE_PREFIX}-cuda${cuda}-base" \
+            --tag ${TAG} \
             "${cuda}/base"
         echo
 
         echo "Pushing images to repository"
-        docker push "${IMAGE_PREFIX}-cuda${cuda}-base"
+        docker push "${TAG}"
         echo
 
-echo "- [\`${IMAGE_PREFIX}-cuda${cuda}-base\` (*${cuda}/base/Dockerfile*)](https://github.com/danieldeutsch/python-cuda/blob/master/${cuda}/base/Dockerfile)" >>README_.md
+echo "- [\`${TAG}\` (*${cuda}/base/Dockerfile*)](https://github.com/danieldeutsch/python-cuda/blob/master/${cuda}/base/Dockerfile)" >>README_.md
 echo "" >>README_.md
 
     done
